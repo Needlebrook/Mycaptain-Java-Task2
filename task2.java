d4410772eec66d18853fd528d812a5c868a4a494
Needlebrook <celiavictor101@gmail.com>
2025-06-15 16:26:10 +0530

    wrote sorting algorithm

--- task2.java
+++ task2.java
@@ -14,6 +14,26 @@ class task2{
         }
         System.out.println("\n");
         
+        System.out.println("Enter 5 integers");
+        
+        for(int x=0;x<arr.length-1;x++){
+            for (int y=0; y<arr.length-1-x;y++){
+                if (arr[x]>arr[x+1]){
+                    int temp = arr[x];
+                    arr[x]= arr[x+1];
+                    arr[x+1]=temp;
+                }
+            }    
+        }
+        System.out.println("The sorted array is: ");
+        for (int i=0;i<5;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
+        System.out.println("The second highest number is : "+arr[3]);
+        System.out.println("The second lowest number is : "+arr[1]);
+
+        scan.close();
     }
 }
 
