c0a4b93aa19ffa075bf4d973c09d6f89fc3f7e43
Needlebrook <celiavictor101@gmail.com>
2025-06-15 16:17:47 +0530

    array reading code written

--- task2.java
+++ task2.java
@@ -4,6 +4,15 @@ class task2{
     public static void main(String[] args) {
         int[] arr = new int[5];
         Scanner scan =new Scanner(System.in);
+
+        for(int i=0;i<5;i++){
+            arr[i] = scan.nextInt();
+        }
+        System.out.println("The numbers you entered are: ");
+        for (int i=0;i<5;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
         
     }
 }
