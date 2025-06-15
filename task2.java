5da35a1c1767a92603307981de0b3e7093c325b1
Needlebrook <celiavictor101@gmail.com>
2025-06-15 16:14:16 +0530

    First draft

---
+++ task2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class task2{
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner scan =new Scanner(System.in);
+        
+    }
+}
+
